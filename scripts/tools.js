810b9bfb9e2974b43f1a468b8c9617b5194d2e91
dlaboy <diego.laboy1@upr.edu>
2024-06-26 10:08:13 -0400

    deploying contact form

--- scripts/tools.js
+++ scripts/tools.js
@@ -3,7 +3,11 @@ document.getElementById('copyButton').addEventListener('click', function() {
     var copyText = document.getElementById('copyInput');
     copyText.select();
     copyText.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand('copy');
-    alert('Copied the text: ' + copyText.value);
+       // Use the Clipboard API to copy text
+       navigator.clipboard.writeText(copyText.value).then(function() {
+        alert('Copied the text: ' + copyText.value);
+    }).catch(function(error) {
+        alert('Failed to copy text: ' + error);
+    });
   });
   
